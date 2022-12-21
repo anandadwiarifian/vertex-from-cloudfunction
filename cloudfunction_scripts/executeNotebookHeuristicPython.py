384b254d91741c8ad10575802669adabf3db29bb
Ananda Dwi <anandadwiarifin@gmail.com>
2022-12-21 16:22:45 +0700

    update

--- cloudfunction_scripts/executeNotebookHeuristicPython.py
+++ cloudfunction_scripts/executeNotebookHeuristicPython.py
@@ -1,28 +1,27 @@
-def executeNotebookCustomModel(
-    template_uri: str, project_id: str, target_bucket: str, 
-    model_uri: str, scaler_uri: str, organization_id: str,
+def executeNotebookHeuristicPython(
+    python_script_path: str, project_id: str, target_bucket: str, 
+    organization_id: str, feature_list: list, audience_id: int,
     bucket_pipeline: str, datafoundation_table: str, topic_id: str,
-    audience_id: int
+    msisdn_sample_path: str, max_msisdn_count: int
 ):
     import gcsfs
     import nbformat
     from nbconvert.preprocessors import ExecutePreprocessor
 
     fs = gcsfs.GCSFileSystem()
-    with fs.open(template_uri, "r") as f:
+    with fs.open(python_script_path, "r") as f:
         str_nb = f.read()
 
-    str_nb = str_nb.replace('{{TEMPLATE_URI}}', template_uri)
     str_nb = str_nb.replace('{{PROJECT_ID}}', project_id)
     str_nb = str_nb.replace('{{TARGET_BUCKET}}', target_bucket)
     str_nb = str_nb.replace('{{ORGANIZATION_ID}}', organization_id)
-    str_nb = str_nb.replace('{{BUCKET_PIPELINE}}', bucket_pipeline)
     str_nb = str_nb.replace('{{DATAFOUNDATION_TABLE}}', datafoundation_table)
-    str_nb = str_nb.replace('{{MODEL_URI}}', model_uri)
-
-    str_nb = str_nb.replace('{{SCALER_URI}}', scaler_uri)
+    str_nb = str_nb.replace('{{BUCKET_PIPELINE}}', bucket_pipeline)
+    str_nb = str_nb.replace('{{FEATURE_LIST}}', str(feature_list))
     str_nb = str_nb.replace('{{AUDIENCE_ID}}', str(audience_id))
     str_nb = str_nb.replace('{{TOPIC_ID}}', topic_id)
+    str_nb = str_nb.replace('{{MAX_MSISDN_COUNT}}', str(max_msisdn_count))
+    str_nb = str_nb.replace('{{MSISDN_SAMPLE_PATH}}', msisdn_sample_path)
 
     nb = nbformat.reads(str_nb, nbformat.NO_CONVERT)
 
--- cloudfunction_scripts/main.py
+++ cloudfunction_scripts/main.py
@@ -1,8 +1,12 @@
 import base64
 import json
 import logging
-from createDataprocJob import createDataprocJob
+import os
 from executeNotebookCustomModel import executeNotebookCustomModel
+from executeNotebookHeuristicPython import executeNotebookHeuristicPython
+# from datetime import date, timedelta
+# from datetime import datetime as dt
+# from dateutil.relativedelta import relativedelta
 
 def hello_pubsub(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
@@ -11,31 +15,50 @@ def hello_pubsub(event, context):
          context (google.cloud.functions.Context): Metadata for the event.
     """
     try:
+        PROJECT_ID = os.environ.get('GCP_PROJECT')
+        TARGET_BUCKET = os.environ.get('TARGET_BUCKET')
+        DATAFOUNDATION_TABLE = os.environ.get('DATAFOUNDATION_TABLE')
+        BUCKET_PIPELINE = os.environ.get('BUCKET_PIPELINE')
+        TOPIC_ID = os.environ.get('TOPIC_ID')
+
         pubsub_message = json.loads(base64.b64decode(event['data']))
+        ORGANIZATION_ID = pubsub_message["organization_id"]
+
+        ## query to CloudSQL to get parameters: model uri, script uri, etc
+        ##
+        ##
+        ## 
+
+        AUDIENCE_ID = 1
+        PYTHON_SCRIPT_PATH = os.environ.get('PYTHON_SCRIPT_PATH')
         
+        BUCKET_PIPELINE_AUDIENCE = BUCKET_PIPELINE+"/"+ORGANIZATION_ID
+
         MODEL_TYPE = pubsub_message["model_type"]
         if MODEL_TYPE == "custom_model":
-            TEMPLATE_URI = pubsub_message["template_uri"]
-            PROJECT_ID = pubsub_message["project_id"]
-            TARGET_TABLE = pubsub_message["target_table"]
+            SCORING_SCRIPT = pubsub_message["scoring_script"]
             MODEL_URI = pubsub_message["model_uri"]
             SCALER_URI = pubsub_message.get("scaler_uri", None)
-
             executeNotebookCustomModel(
-                template_uri=TEMPLATE_URI, project_id=PROJECT_ID,
-                target_table=TARGET_TABLE, model_uri=MODEL_URI,
-                scaler_uri=SCALER_URI
+                template_uri=SCORING_SCRIPT, project_id=PROJECT_ID,
+                datafoundation_table=DATAFOUNDATION_TABLE, bucket_pipeline=BUCKET_PIPELINE_AUDIENCE,
+                target_bucket=TARGET_BUCKET, model_uri=MODEL_URI, 
+                scaler_uri=SCALER_URI, organization_id=ORGANIZATION_ID,
+                topic_id=TOPIC_ID, audience_id=AUDIENCE_ID,
             )
-        elif MODEL_TYPE == "generic_model_pyspark":
-            PROJECT_ID = pubsub_message["project_id"]
-            PYSPARK_SCRIPT = pubsub_message["pyspark_script"]
-            CUSTOM_IMAGE_TAG = pubsub_message["custom_image_tag"]
-            PARAMETERS = pubsub_message["parameters"]
 
-            createDataprocJob(
-                project_id=PROJECT_ID, pyspark_script=PYSPARK_SCRIPT, 
-                custom_image_tag=CUSTOM_IMAGE_TAG, parameters=PARAMETERS
+        elif MODEL_TYPE == "generic_model_python":
+            FEATURE_LIST = pubsub_message["feature_list"]
+            MSISDN_SAMPLE_PATH = pubsub_message["msisdn_sample_path"]
+            MAX_MSISDN_COUNT = pubsub_message["max_msisdn_count"]
+            executeNotebookHeuristicPython(
+                project_id=PROJECT_ID, python_script_path=PYTHON_SCRIPT_PATH, 
+                datafoundation_table=DATAFOUNDATION_TABLE, bucket_pipeline=BUCKET_PIPELINE_AUDIENCE,
+                organization_id=ORGANIZATION_ID, feature_list=FEATURE_LIST, msisdn_sample_path=MSISDN_SAMPLE_PATH,
+                max_msisdn_count=MAX_MSISDN_COUNT, topic_id=TOPIC_ID, audience_id=AUDIENCE_ID,
+                target_bucket=TARGET_BUCKET
             )
+          
         else:
             raise Exception("model_type is not recognized")
                        
